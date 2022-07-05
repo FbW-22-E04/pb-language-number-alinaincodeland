08fc0f09a54d149cdce8461ea33592f27df2dfe8
dci-student <alinacuznetov@gmail.com>
2022-07-05 12:11:57 +0200

    strings and numbers

---
+++ solution.js
@@ -0,0 +1,18 @@
+/* 1 */
+
+let introSentence = "hello, my name is Fran and I am";
+let age = 25; 
+let result = introSentence.concat(" ", age);
+
+- + operator would skip the space. 
+
+/* 2 */
+
+let integer = "1005" ; 
+let number = + integer;
+
+
+let float = "10.05";
+let number = + float;
+
+
